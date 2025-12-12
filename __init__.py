c4256e699c1533e3388b0bbd34d9e56bf6602847
Kirill Emelianov <kirill.emelianov@gcore.com>
2025-12-12 16:03:03 +0400

    Remove comfyui dependency

--- __init__.py
+++ __init__.py
@@ -4,6 +4,5 @@ Official SeedVR2 integration for ComfyUI
 """
 
 from .src.optimization.compatibility import ensure_triton_compat  # noqa: F401
-from .src.interfaces import comfy_entrypoint, SeedVR2Extension
 
-__all__ = ["comfy_entrypoint", "SeedVR2Extension"]
+__all__ = ['ensure_triton_compat']
